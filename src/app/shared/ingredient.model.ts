33e71e432a4069d04154e48f18d873f45bf9aa49
Elinex <eline.ax@gmail.com>
2018-12-17 22:10:35 -0200

    changed recipe detaill view

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  
+  constructor(public name: string, public amount: number){}
+
+  // THE CONSTRUCTOR METHOD ABOVE IS A SHORTCUT OF THE BELOW:
+
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number){
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+}
